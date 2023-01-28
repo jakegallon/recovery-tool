a5305eb2fa026e7d1b7118cecd6c19d8dadb52b9
jakegallon <jakegallon83@gmail.com>
2023-01-28 18:59:52 +0000

    Add Utility method to read byte Array as Int

--- src/Utility.java
+++ src/Utility.java
@@ -5,6 +5,17 @@ import java.math.BigInteger;
 
 public class Utility {
 
+    public static int byteArrayToInt(byte[] bytes, boolean isReversed) {
+        if(isReversed) {
+            for(int i = 0; i < bytes.length / 2; i++) {
+                byte b = bytes[i];
+                bytes[i] = bytes[bytes.length - i - 1];
+                bytes[bytes.length - i - 1] = b;
+            }
+        }
+        return new BigInteger(bytes).intValueExact();
+    }
+
     public static long byteArrayToLong(byte[] bytes, boolean isReversed) {
         if(isReversed) {
             for(int i = 0; i < bytes.length / 2; i++) {
