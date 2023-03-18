dc7557a391871f5080ba04920a42c884b531f0fb
jakegallon <jakegallon83@gmail.com>
2023-03-18 17:14:48 +0000

    Add functionality to print all deleted file names on drive to console in ScanPanel.java

--- src/ScanPanel.java
+++ src/ScanPanel.java
@@ -1,3 +1,6 @@
+import java.io.*;
+import java.nio.channels.FileChannel;
+
 public class ScanPanel extends StepPanel {
 
     @Override
@@ -15,6 +18,33 @@ public class ScanPanel extends StepPanel {
     }
 
     public ScanPanel() {
+        try {
+            printAllFileNames();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
+    private void printAllFileNames() throws IOException {
+        NTFSInformation ntfsInformation = NTFSInformation.getInstance();
+        long mftSizeBytes = ntfsInformation.getMFTSizeBytes();
+        int mftRecordLength = ntfsInformation.getMFTRecordLength();
+        long mftRecordsInMFT = mftSizeBytes/mftRecordLength;
+
+        RandomAccessFile diskAccess = new RandomAccessFile(ntfsInformation.getRoot(), "r");
+        FileChannel diskChannel = diskAccess.getChannel();
+
+        long offset = ntfsInformation.getMFTByteLocation();
+        for(int i = 0; i < mftRecordsInMFT; i++) {
+            byte[] mftRecordBytes = Utility.readMFTRecord(diskChannel, offset);
+            if (mftRecordBytes != null) {
+                MFTRecord mftRecord = new MFTRecord(mftRecordBytes);
+                if(mftRecord.isDeleted()){
+                    System.out.println(mftRecord.getFileName());
+                }
+            }
+            offset += mftRecordLength;
+        }
+        diskAccess.close();
     }
 }
