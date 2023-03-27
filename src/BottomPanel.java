559bccc4085810cbb276b5a687352933e8fe40b1
jakegallon <jakegallon83@gmail.com>
2023-03-27 18:51:58 +0100

    Refactor BottomPanel

--- src/BottomPanel.java
+++ src/BottomPanel.java
@@ -9,41 +9,36 @@ public class BottomPanel extends JPanel {
 
     public BottomPanel() {
         setBorder(new MatteBorder(0, 2, 2, 2, Frame.DARK_COLOR));
+        setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
+        setPreferredSize(new Dimension(100, 40));
 
-        SpringLayout springLayout = new SpringLayout();
-        setLayout(springLayout);
+        add(Box.createHorizontalStrut(8));
+        addCancelButton();
 
+        add(Box.createHorizontalGlue());
+
+        addBackButton();
+        add(Box.createHorizontalStrut(8));
+        addNextButton();
+        add(Box.createHorizontalStrut(8));
+    }
+
+    private void addCancelButton() {
         JButton cancelButton = new JButton("Cancel");
         cancelButton.addActionListener(e -> System.exit(0));
         add(cancelButton);
+    }
 
-        int gap = 8;
-        int buttonWidth = 120;
-
-        springLayout.putConstraint(SpringLayout.WEST, cancelButton, gap, SpringLayout.WEST, this);
-        springLayout.putConstraint(SpringLayout.EAST, cancelButton, buttonWidth, SpringLayout.WEST, cancelButton);
-        springLayout.putConstraint(SpringLayout.NORTH, cancelButton, gap, SpringLayout.NORTH, this);
-        springLayout.putConstraint(SpringLayout.SOUTH, cancelButton, -gap, SpringLayout.SOUTH, this);
-
+    private void addNextButton() {
         nextButton.addActionListener(e -> Frame.nextStep());
         nextButton.setEnabled(false);
         add(nextButton);
+    }
 
-        springLayout.putConstraint(SpringLayout.EAST, nextButton, -gap, SpringLayout.EAST, this);
-        springLayout.putConstraint(SpringLayout.WEST, nextButton, -buttonWidth, SpringLayout.EAST, nextButton);
-        springLayout.putConstraint(SpringLayout.NORTH, nextButton, gap, SpringLayout.NORTH, this);
-        springLayout.putConstraint(SpringLayout.SOUTH, nextButton, -gap, SpringLayout.SOUTH, this);
-
+    private void addBackButton() {
         backButton.addActionListener(e -> Frame.backStep());
         backButton.setEnabled(false);
         add(backButton);
-
-        springLayout.putConstraint(SpringLayout.EAST, backButton, -gap, SpringLayout.WEST, nextButton);
-        springLayout.putConstraint(SpringLayout.WEST, backButton, -buttonWidth, SpringLayout.EAST, backButton);
-        springLayout.putConstraint(SpringLayout.NORTH, backButton, gap, SpringLayout.NORTH, this);
-        springLayout.putConstraint(SpringLayout.SOUTH, backButton, -gap, SpringLayout.SOUTH, this);
-
-        setPreferredSize(new Dimension(100, 40));
     }
 
     public static void setNextButtonEnabled(boolean b) {
