3b99f70e97de650ea939ad514ff5297b94c1e1c1
jakegallon <jakegallon83@gmail.com>
2023-03-30 20:33:50 +0100

    Add the ability to recover files with non-resident data

--- src/RecoveryPanel.java
+++ src/RecoveryPanel.java
@@ -3,11 +3,16 @@ import javax.swing.border.EmptyBorder;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
 import java.util.*;
 
 public class RecoveryPanel extends StepPanel {
 
-    DetailedProgressBar recoveryProgressBar = new DetailedProgressBar();
+    private final DetailedProgressBar recoveryProgressBar = new DetailedProgressBar();
+    private final File outputDirectory;
+    private int filesProcessed = 0;
 
     @Override
     public void onNextStep() {
@@ -22,6 +27,7 @@ public class RecoveryPanel extends StepPanel {
     private final ArrayList<MFTRecord> mftRecords;
     public RecoveryPanel(ArrayList<MFTRecord> mftRecords) {
         this.mftRecords = mftRecords;
+        outputDirectory = PartitionPanel.getOutput();
 
         setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
         setBorder(new EmptyBorder(10, 10, 10, 10));
@@ -41,30 +47,65 @@ public class RecoveryPanel extends StepPanel {
 
     private void recoverFiles() {
         for(MFTRecord mftRecord : mftRecords) {
-            mftRecord.parseDataAttribute();
-            if(mftRecord.isDataResident()) {
-                recoverResidentFile(mftRecord);
-            } else {
-                recoverNonResidentFile(mftRecord);
+            try {
+                mftRecord.parseDataAttribute();
+                if (mftRecord.isDataResident()) {
+                    recoverResidentFile(mftRecord);
+
+                } else {
+                    recoverNonResidentFile(mftRecord);
+
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
             }
+            filesProcessed ++;
+            recoveryProgressBar.setValue(filesProcessed);
+            recoveryProgressBar.updateInformationLabels();
         }
     }
 
-    private void recoverResidentFile(MFTRecord mftRecord) {
+    private void recoverResidentFile(MFTRecord mftRecord) throws IOException {
         byte[] fullBytes = mftRecord.getAttribute(Attribute.DATA);
         byte[] dataBytes = Arrays.copyOfRange(fullBytes, 0x18, 0x18 + (int) mftRecord.getFileSizeBytes());
         String fileName = mftRecord.getFileName();
 
-        try {
-            File newFile = new File(PartitionPanel.getOutput(), fileName);
-            FileOutputStream fos = new FileOutputStream(newFile);
-            fos.write(dataBytes);
-            fos.close();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        File newFile = new File(outputDirectory, fileName);
+        FileOutputStream fos = new FileOutputStream(newFile);
+        fos.write(dataBytes);
+        fos.close();
     }
 
-    private void recoverNonResidentFile(MFTRecord mftRecord) {
+    private void recoverNonResidentFile(MFTRecord mftRecord) throws IOException {
+        NTFSInformation ntfsInformation = NTFSInformation.getInstance();
+        final int bytesPerCluster = ntfsInformation.getBytesPerCluster();
+        File root = ntfsInformation.getRoot();
+
+        String fileName = mftRecord.getFileName();
+        File newFile = new File(outputDirectory, fileName);
+        FileOutputStream fos = new FileOutputStream(newFile);
+
+        HashMap<Long, Long> dataRunOffsetClusters = mftRecord.getDataRunOffsetClusters();
+        for(Map.Entry<Long, Long> dataRun : dataRunOffsetClusters.entrySet()) {
+            long dataRunOffsetBytes = dataRun.getKey();
+            long dataRunLengthClusters = dataRun.getValue();
+
+            RandomAccessFile diskAccess = new RandomAccessFile(root, "r");
+            FileChannel diskChannel = diskAccess.getChannel();
+
+            long internalOffsetBytes = 0L;
+            for (long i = 0L; i < dataRunLengthClusters; i++) {
+                diskChannel.position(dataRunOffsetBytes + internalOffsetBytes);
+                byte[] thisCluster = new byte[bytesPerCluster];
+                ByteBuffer buffer = ByteBuffer.wrap(thisCluster);
+
+                diskChannel.read(buffer);
+                fos.write(thisCluster);
+
+                internalOffsetBytes += bytesPerCluster;
+            }
+            diskAccess.close();
+        }
+        fos.close();
     }
 }
